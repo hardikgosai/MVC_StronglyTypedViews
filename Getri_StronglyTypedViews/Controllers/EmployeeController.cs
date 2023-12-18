c4b1e71013d9291edafbdd1199ba4a155c8d4bdf
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-18 10:20:50 +0530

    Add project files.

---
+++ Getri_StronglyTypedViews/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Getri_StronglyTypedViews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Getri_StronglyTypedViews.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee emp = new Employee()
+            {
+                EmpId = 1,
+                EmpName = "John",
+                EmpSal = 10000
+            };
+
+            return View(emp);            
+        }
+    }
+}
---
+++ Getri_StronglyTypedViews/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Getri_StronglyTypedViews.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+
+        public string EmpName { get; set; }
+
+        public float EmpSal { get; set; }
+    }
+}
