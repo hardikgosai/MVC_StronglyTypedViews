2ca043d4a78309ff9f622397ab9ff005788cd3cf
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-18 10:34:09 +0530

    New models added

---
+++ Getri_StronglyTypedViews/Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using Getri_StronglyTypedViews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Getri_StronglyTypedViews.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer()
+                {
+                    CustId = 1,
+                    CustName = "John",
+                    CustAddress = "London",
+                    CustPhoneNo = "1234567890"
+                },
+                new Customer()
+                {
+                    CustId = 2,
+                    CustName = "Smith",
+                    CustAddress = "New York",
+                    CustPhoneNo = "9876543210"
+                },
+                new Customer()
+                {
+                    CustId = 3,
+                    CustName = "Mark",
+                    CustAddress = "Sydney",
+                    CustPhoneNo = "1234509876"
+                },
+                new Customer()
+                {
+                    CustId = 4,
+                    CustName = "Chris",
+                    CustAddress = "Paris",
+                    CustPhoneNo = "6789054321"
+                }
+            };
+
+            return View(customers);
+        }
+    }
+}
---
+++ Getri_StronglyTypedViews/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace Getri_StronglyTypedViews.Models
+{
+    public class Customer
+    {
+        public int CustId { get; set; }
+
+        public string CustName { get; set; }
+
+        public string CustAddress { get; set; }
+
+        public string CustPhoneNo { get; set; }
+    }
+}
