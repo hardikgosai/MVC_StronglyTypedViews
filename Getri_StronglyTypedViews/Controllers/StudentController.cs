c09eee4c98b5412dfbd825c904099a58570329b6
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-18 10:49:37 +0530

    New Class added

---
+++ Getri_StronglyTypedViews/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Getri_StronglyTypedViews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Getri_StronglyTypedViews.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+           List<Student> list = new List<Student>();
+
+            Student student1 = new Student();
+            student1.StudentId = 1;
+            student1.StudentName = "John";
+            student1.StudentBranch = "CSE";
+            student1.StudentSection = "A";
+            student1.StudentGender = "Male";
+            list.Add(student1);
+
+            Student student2 = new Student();
+            student2.StudentId = 2;
+            student2.StudentName = "Smith";
+            student2.StudentBranch = "ECE";
+            student2.StudentSection = "B";
+            student1.StudentGender = "Female";
+            list.Add(student2);
+
+            Student student3 = new Student();
+            student3.StudentId = 3;
+            student3.StudentName = "Mark";
+            student3.StudentBranch = "EEE";
+            student3.StudentSection = "C";
+            student1.StudentGender = "Male";
+            list.Add(student3);
+
+
+            return View(list);
+        }
+    }
+}
---
+++ Getri_StronglyTypedViews/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace Getri_StronglyTypedViews.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string StudentName { get; set; }
+        
+        public string StudentBranch { get; set; }
+
+        public string StudentSection { get; set;}
+
+        public string StudentGender { get; set;}
+    }    
+}
